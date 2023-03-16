280602b60047da1fe2e2903f268a5864731a81a5
Carlos Bekinho <carlos.dev.job@gmail.com>
2023-03-15 21:47:14 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,85 @@
+let openBtn = document.querySelector("#open-btn")
+let closeBtn = document.querySelector("#close-btn")
+let headerList = document.querySelector(".header-list")
+
+let formSubmitBtn = document.querySelector("#enviar-btn")
+
+openBtn.addEventListener("click", openMenu)
+closeBtn.addEventListener("click", closeMenu)
+
+
+
+
+function openMenu() {
+    headerList.style.top = "-4.3%"
+    openBtn.style.display = 'none'
+    closeBtn.style.display = "block"  
+}
+
+function closeMenu() {
+    headerList.style.top = '-200%'
+    closeBtn.style.display = "none"
+    openBtn.style.display = 'block'
+}
+
+function clickMenu() {
+    headerList.style.top = '-200%'
+    closeBtn.style.display = "none"
+    openBtn.style.display = 'block'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+formSubmitBtn.addEventListener("click", submitBtn)
+
+function submitBtn() {
+
+    let formInputName = document.querySelector(".input-nome")
+    let inputNameValue = formInputName.value
+
+    let formInputEmail = document.querySelector(".input-email")
+    let inputEmailValue = formInputEmail.value
+
+    let formInputMsg = document.querySelector(".input-msg")
+    let inputMsgValue = formInputMsg.value
+
+
+    if (inputNameValue.length === 0 && inputEmailValue.length === 0 && inputMsgValue.length === 0) {
+        formInputName.setAttribute('id', 'error')
+        formInputEmail.setAttribute('id', 'error')
+        formInputMsg.setAttribute('id', 'error')
+    }
+}
+
+
+
+
+
+function inputBlur() {
+
+    let formInputName = document.querySelector(".input-nome")
+    let formInputEmail = document.querySelector(".input-email")
+    let formInputMsg = document.querySelector(".input-msg")
+    
+
+    formInputName.removeAttribute('id', 'error')
+    formInputEmail.removeAttribute('id', 'error')
+    formInputMsg.removeAttribute('id', 'error')
+
+
+}
