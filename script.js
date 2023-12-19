0e5f3c5f16c4e609b31d01e27af4b718ca2d7ba5
Carlos Bekinho <carlos.dev.job@gmail.com>
2023-12-18 22:52:55 -0300

    update new project

--- script.js
+++ script.js
@@ -69,3 +69,28 @@ window.addEventListener('scroll', function() {
     }
   });
   
+
+//   img slide projetos
+
+let slideIndex = 0;
+
+// Função para mostrar os slides automaticamente
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Altere 2000 para o intervalo desejado em milissegundos (2 segundos neste exemplo)
+}
+
+// Inicie o slideshow quando a página for carregada
+window.onload = function () {
+  showSlides();
+};
+
